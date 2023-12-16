8684be06c72d34d2ab15ae2f709328e4466fe483
Dmytro Zuiev <zuyevdmytro@gmail.com>
2023-12-16 14:37:45 +0000

    Commit for bash script

--- beta/printf/find_used_libft_funcs.sh
+++ beta/printf/find_used_libft_funcs.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Путь к исходным файлам ft_printf
+# Path to ft_printf sources
 PRINTF_SRCS="src/*.c"
 
-# Путь к libft
+# Path- to libft sources
 LIBFT_DIR="../libft"
 
-# Найти все вызовы функций libft в исходных файлах ft_printf
+# Find all used libft functions in ft_printf
 USED_LIBFT_FUNCS=$(grep -oh "ft_[a-zA-Z_]*" $PRINTF_SRCS | sort | uniq)
 
-# Печатаем список файлов .c, соответствующих этим функциям
+# Print all used libft functions with their source file
 for func in $USED_LIBFT_FUNCS; do
-    src_file="$LIBFT_DIR/${func}.c"
-    if [ -f "$src_file" ]; then
-        echo "$src_file"
-    fi
+	src_file="$LIBFT_DIR/${func}.c"
+	if [ -f "$src_file" ]; then
+		echo "$src_file"
+	fi
 done
